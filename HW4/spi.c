b75026652370d962fb081a06ae9ed17020542b55
connormurray271 <connormurray@dhcp-10-105-21-159.wireless.northwestern.private>
2016-04-21 02:20:39 -0500

    spi initialized

--- HW4/spi.c
+++ HW4/spi.c
@@ -3,5 +3,11 @@ void initSPI1(){
   SDI1Rbits.SDI1R = 0b0000;   //assign SDI1 to A1
   SS1Rbits.SS1R = 0b0000;     //assign SS1 to A0
 
-  
+  SPI1BUF;                   //clear rx buffer by reading from it
+  SPI1BRG = 1;              //set clock frequency to 2 MHz
+  SPI1STATbits.SPIROV = 0;  //clear overflow bit
+  SPI1CONbits.MSTEN = 1;    //master enabled
+  SPI1CONbits.MSSEN = 0;    //auto slave select diabled
+  SPI1CONbits.ON = 1;       //turn on SPI1
+
 }
