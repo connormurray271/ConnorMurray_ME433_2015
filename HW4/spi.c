cc37abc961e083712dc54c8b381517e260a338d5
connormurray271 <connormurray@dhcp-10-105-21-159.wireless.northwestern.private>
2016-04-21 02:45:44 -0500

    spi code written but not tested

--- HW4/spi.c
+++ HW4/spi.c
@@ -1,7 +1,7 @@
-void initSPI1(){
+void initSPI1(void){
   RPB8Rbits.RPB8R = 0b0011;    //assign SDO1 to B8
+  RPB7Rbits.RPB7R = 0b0011;    //assign SS1 to B7
   SDI1Rbits.SDI1R = 0b0000;   //assign SDI1 to A1
-  SS1Rbits.SS1R = 0b0000;     //assign SS1 to A0
 
   SPI1BUF;                   //clear rx buffer by reading from it
   SPI1BRG = 1;              //set clock frequency to 2 MHz
@@ -10,4 +10,11 @@ void initSPI1(){
   SPI1CONbits.MSSEN = 0;    //auto slave select diabled
   SPI1CONbits.ON = 1;       //turn on SPI1
 
+  TRISBbits.TRISB7 = 0;     //set SSI pin (B7) as output
+  LATBbits.LATB7 = 1;       //do not allow communication
+}
+
+
+char SPI1_IO(char write){
+  SPI1BUF = write;        //send to buffer
 }
