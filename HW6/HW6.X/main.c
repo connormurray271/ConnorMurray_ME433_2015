6f9b51f6caae3ae8c0b794795e3c529a776b2102
connormurray271 <connormurray@dhcp-10-105-117-249.wireless.northwestern.private>
2016-05-02 02:55:04 -0500

    50% pwm set

--- HW6/HW6.X/main.c
+++ HW6/HW6.X/main.c
@@ -1,7 +1,9 @@
+#include <xc.h>
 #include "pic_setup.h"
 #include "i2c.h"
 #include "imu.h"
-#include <stdio.h>
+
+#define ADDRESS 0b1101011   //IMU address
 
 int main() {
 
@@ -24,19 +26,47 @@ int main() {
     initIMU();
 
     __builtin_enable_interrupts();
+    char length = 14;
+    unsigned char data[14];
 
-    // unsigned char data[];
+    //Timer2
+    T2CONbits.TCKPS = 0b111;        //set prescaler to 256
+    PR2 = 3749;                     //period = (PR2 + 1) * N * 1/48e6 = 50 Hz
+    TMR2 = 0;                       //initial timer count is 0
+    T2CONbits.ON = 1;               //turn on Timer2
 
-    char r = I2C_read_WHOAMI();
-    TRISAbits.TRISA4 = 0;    //make A4 an output
-    LATAbits.LATA4 = 0;      //turn on A4 LED
+    //OC1
+    RPA0Rbits.RPA0R = 0b0101;       //set OC1 on A0
+    OC1CONbits.OCM = 0b110;         //PWM mode without fault pin
+    OC1RS = 1875;                   //set duty cycle 50%
+    OC1R = 1875;
+    OC1CONbits.ON = 1;              //turn on OC1
 
-    if(r == 0b01101001){
-      LATAbits.LATA4 = 1;      //turn on A4 LED
-    }
+
+    //OC2
+    RPA1Rbits.RPA1R = 0b0101;       //set OC2 on A1
+    OC2CONbits.OCM = 0b110;         //PWM mode without fault pin
+    OC2RS = 1875;                   //set duty cycle 50%
+    OC2R = 1875;
+    OC2CONbits.ON = 1;              //turn on OC2
+
+
 
     while(1) {
-      // I2C_read_multiple(ADDRESS, 0x20, data, 14);
+      I2C_read_multiple(ADDRESS, 0x20, data, length);
+
+      short temp = (data[2] << 8) | data[1];
+
+      short gyro_x = (data[4] << 8) | data[3];
+      short gyro_y = (data[6] << 8) | data[5];
+      short gyro_z = (data[8] << 8) | data[7];
+
+      short accel_x = (data[10] << 8) | data[9];
+      short accel_y = (data[12] << 8) | data[11];
+      short accel_z = (data[14] << 8) | data[13];
+
+      // OC1RS = accel_x;
+      // OC2RS = accel_y;
     }
 
 
