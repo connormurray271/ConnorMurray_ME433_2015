4535eed57e2765dff2828788ff2e4479fb52efd5
connormurray271 <connormurray@dhcp-10-105-7-25.wireless.northwestern.private>
2016-06-09 16:15:29 -0500

    finishing commit

--- motor.X/main.c
+++ motor.X/main.c
@@ -21,32 +21,32 @@ int main() {
 
     __builtin_enable_interrupts();
 
-    T2CONbits.TCKPS = 0b111;        //set prescaler to 256
-    PR2 = 3749;                     //period = (PR2 + 1) * N * 1/48e6 = 50 Hz
+    T2CONbits.TCKPS = 0b011;        //set prescaler to 256
+    PR2 = 5999;                     //period = (PR2 + 1) * N * 1/48e6 = 1kHz Hz
     TMR2 = 0;                       //initial timer count is 0
     T2CONbits.ON = 1;               //turn on Timer2
 
     RPA0Rbits.RPA0R = 0b0101;       //set OC1 on A0
     OC1CONbits.OCM = 0b110;         //PWM mode without fault pin
-    OC1RS = 1875;                   //set duty cycle 50%
-    OC1R = 1875;
+    OC1RS = 3000;                   //set duty cycle 50%
+    OC1R = 3000;
     OC1CONbits.ON = 1;              //turn on OC1
 
     TRISAbits.TRISA4 = 0;
 
     RPB8Rbits.RPB8R = 0b0101;       //set OC2 on B8
     OC2CONbits.OCM = 0b110;         //PWM mode without fault pin
-    OC2RS = 3749;                   //set duty cycle 50%
-    OC2R = 3749;
+    OC2RS = 3000;                   //set duty cycle 50%
+    OC2R = 3000;
     OC2CONbits.ON = 1;              //turn on OC2
 
     TRISBbits.TRISB7 = 0;
 
     while(1) {
-        OC1RS = 3749;
+        OC1RS = 3000;
         LATAbits.LATA4 = 0;
 
-        OC2RS = 3749;
+        OC2RS = 3000;
         LATBbits.LATB4 = 0;
     }
 
